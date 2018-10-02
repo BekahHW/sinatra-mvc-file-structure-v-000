2e379ef6a1e5443c73084267237dc88d6dc4d9a4
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-10-02 09:30:30 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,17 @@
 class Model
+  
+  
+  
+  
+  class Dog
+    attr_accessor :name, :breed, :age
+  
+    def initialize(name, breed, age)
+      @name= name
+      @breed = breed
+      @age = age
+    end
+    
+  end
 
 end
