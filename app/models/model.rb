e91ec4024937d500160ef70c9df69bb5e0f4c308
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-10-02 09:32:17 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -5,11 +5,14 @@ class Model
   
   class Dog
     attr_accessor :name, :breed, :age
+    
+    @@all=[]
   
     def initialize(name, breed, age)
       @name= name
       @breed = breed
       @age = age
+      @@all<<self
     end
     
   end
