0aafacb9c94a9a0a66d432269eae172d41cff373
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-10-02 09:51:15 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,22 +1,22 @@
-require 'pry'
-class Model
+# require 'pry'
+# class Model
 
   
-  class Dog
-    attr_accessor :name, :breed, :age
+#   class Dog
+#     attr_accessor :name, :breed, :age
     
-    @@all=[]
+#     @@all=[]
   
-    def initialize(name, breed, age)
-      @name= name
-      @breed = breed
-      @age = age
-      @@all<<self
-    end
+#     def initialize(name, breed, age)
+#       @name= name
+#       @breed = breed
+#       @age = age
+#       @@all<<self
+#     end
     
-    def self.all
-      @@all
-    end
+#     def self.all
+#       @@all
+#     end
 
-  end
-end
+#   end
+# end
